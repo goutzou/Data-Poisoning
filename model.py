85157a3336bdddf4b1220966b85928539f9a5249
goutzou <ngoutzoulias@gmail.com>
2024-11-18 12:00:12 -0500

    simple model on MNIST

--- model.py
+++ model.py
@@ -1,1 +1,103 @@
-print("hi")
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+transform = transforms.Compose([
+    transforms.ToTensor(), 
+    transforms.Normalize((0.1307,), (0.3081,))  
+])
+
+train_dataset = datasets.MNIST(
+    root='./data',
+    train=True,
+    transform=transform,
+    download=True
+)
+
+test_dataset = datasets.MNIST(
+    root='./data',
+    train=False,
+    transform=transform,
+    download=True
+)
+batch_size = 64
+
+train_loader = torch.utils.data.DataLoader(
+    dataset=train_dataset,
+    batch_size=batch_size,
+    shuffle=True
+)
+
+test_loader = torch.utils.data.DataLoader(
+    dataset=test_dataset,
+    batch_size=1000,
+    shuffle=False
+)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        self.fc1 = nn.Linear(28 * 28, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 10)
+    
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)  
+        return x
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device: {device}')
+
+model = Net().to(device)
+
+criterion = nn.CrossEntropyLoss() 
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()  
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        
+        optimizer.zero_grad()  
+        output = model(data)  
+        loss = criterion(output, target)  
+        loss.backward()  
+        optimizer.step() 
+        
+        if batch_idx % 100 == 0:
+            print(f'Train Epoch: {epoch} '
+                  f'[{batch_idx * len(data)}/{len(train_loader.dataset)} '
+                  f'({100. * batch_idx / len(train_loader):.0f}%)]\t'
+                  f'Loss: {loss.item():.6f}')
+
+def test(model, device, test_loader):
+    model.eval() 
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():  
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            
+            output = model(data)  
+            test_loss += criterion(output, target).item()  
+            
+            pred = output.argmax(dim=1, keepdim=True)  
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    
+    test_loss /= len(test_loader)  
+    
+    print(f'\nTest set: Average loss: {test_loss:.4f}, '
+          f'Accuracy: {correct}/{len(test_loader.dataset)} '
+          f'({100. * correct / len(test_loader.dataset):.2f}%)\n')
+          
+num_epochs = 5  
+for epoch in range(1, num_epochs + 1):
+    train(model, device, train_loader, optimizer, epoch)
+    test(model, device, test_loader)
+
+
